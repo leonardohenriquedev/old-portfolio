eaaf3522ce9cd9f0018c2905e76e9238720e204b
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-09-23 20:34:12 -0300

    adiciona projeto trybewarts

---
+++ projects/trybewarts/script.js
@@ -0,0 +1,35 @@
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const loginButton = document.querySelector('.loginbtt');
+const check = document.getElementById('agreement');
+const botaoEnviar = document.getElementById('submit-btn');
+const textArea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+
+function login(event) {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+loginButton.addEventListener('click', login);
+
+botaoEnviar.disabled = true;
+
+function itsChecked() {
+  if (check.checked === true) {
+    botaoEnviar.disabled = false;
+  } else botaoEnviar.disabled = true;
+}
+
+check.addEventListener('click', itsChecked);
+
+
+counter.innerText = textArea.maxLength;
+textArea.addEventListener('input', () => {
+  const atual = textArea.maxLength;
+  counter.innerText = (atual - textArea.value.length);
+});
